c971b8cf612dabb00bec906ddc76c0dad2ad61fe
Dev-Lucas-Dias <diaslucasaparecido@gmail.com>
2024-09-05 10:55:33 -0300

    espaçamento e javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+function togglemode(){
+    const html = document.documentElement
+    html.classList.toggle("container")
+    const img =document.querySelector("#primary")
+    
+let link = texto.link("./index-2.html");
+document.write(link);
+}
